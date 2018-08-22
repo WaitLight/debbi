2f5e70150f803d54ff2dbd8d2502b71f48920aa8
WaitLight <waitlight520@gmail.com>
2018-08-22 23:51:24 +0800

    尝试 resovle #3

--- debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiArgumentResolvers.java
+++ debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiArgumentResolvers.java
@@ -1,13 +1,13 @@
 package org.dl.debbi.controller.config;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-import java.lang.annotation.Annotation;
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 
 public class DebbiArgumentResolvers implements HandlerMethodArgumentResolver {
 
@@ -18,7 +18,17 @@ public class DebbiArgumentResolvers implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        System.out.println("parameter - -  " + parameter.getParameterName() + " - " + parameter.getParameterType());
+        System.out.println("param name: " + parameter.getParameterName() + ", param type" + parameter.getParameterType());
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        BufferedReader br = request.getReader();
+
+        String str, wholeStr = "";
+        while ((str = br.readLine()) != null) {
+            wholeStr += str;
+        }
+        System.out.println(">> " + wholeStr);
+
         return null;
     }
 }
