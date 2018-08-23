aee089f74354e102f6f3059b861b8405f3fb599c
WaitLight <waitlight520@gmail.com>
2018-08-23 11:21:17 +0800

    统一字符串处理 resolve #3

--- debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiArgumentResolvers.java
+++ debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiArgumentResolvers.java
@@ -1,15 +1,26 @@
 package org.dl.debbi.controller.config;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.MethodParameter;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.BufferedReader;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.nio.charset.Charset;
 
 public class DebbiArgumentResolvers implements HandlerMethodArgumentResolver {
+
+    public static final String BODY = "body";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -18,17 +29,37 @@ public class DebbiArgumentResolvers implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        System.out.println("param name: " + parameter.getParameterName() + ", param type" + parameter.getParameterType());
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-
-        BufferedReader br = request.getReader();
-
-        String str, wholeStr = "";
-        while ((str = br.readLine()) != null) {
-            wholeStr += str;
+        JsonNode body = (JsonNode) request.getAttribute(BODY);
+        if (body == null) {
+            String bodyStr = StreamUtils.copyToString(request.getInputStream(), Charset.forName("UTF-8"));
+            body = objectMapper.readValue(bodyStr, JsonNode.class);
+            request.setAttribute(BODY, body);
         }
-        System.out.println(">> " + wholeStr);
-
-        return null;
+        return convert((body).get(parameter.getParameterName()), parameter.getParameterType());
     }
+
+    private static Object convert(JsonNode json, Type type) throws Exception {
+        if (json == null) return null;
+        try {
+            if (type instanceof ParameterizedType) // for parameterized type
+                return objectMapper.convertValue(json, new TypeReference<>() {
+                    @Override
+                    public Type getType() {
+                        return type;
+                    }
+                });
+            Class aClass = (Class) type;
+            if (aClass == String.class) {
+                String trim = StringUtils.trimWhitespace(json.asText());
+                if (trim.equals("")) return null;
+                return objectMapper.convertValue(trim, aClass); // for class
+            }
+            return objectMapper.convertValue(json, aClass);
+        } catch (Exception e) {
+            throw new Exception("illegal_request_format");
+        }
+    }
+
+
 }
--- debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiWebMvcConfig.java
+++ debbi-controller/src/main/java/org/dl/debbi/controller/config/DebbiWebMvcConfig.java
@@ -1,16 +1,12 @@
 package org.dl.debbi.controller.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
-@EnableAutoConfiguration
 public class DebbiWebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
