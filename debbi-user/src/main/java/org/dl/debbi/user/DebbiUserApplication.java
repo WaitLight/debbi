190a3db6c754634ccf3b885766d828220ddf742b
waitlight <waitlight520@gmail.com>
2019-01-21 23:40:39 +0800

    update

--- debbi-user/src/main/java/org/dl/debbi/user/DebbiUserApplication.java
+++ debbi-user/src/main/java/org/dl/debbi/user/DebbiUserApplication.java
@@ -2,8 +2,10 @@ package org.dl.debbi.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DebbiUserApplication {
 
 	public static void main(String[] args) {
